6f5a59e6fbfee369c3a3f9d980c5580e9c424589
Abhay Chaudhary <abhaycdry10@gmail.com>
2025-06-23 16:22:36 +0545

    subject

---
+++ models/userSubjectModel.js
@@ -0,0 +1,9 @@
+// models/userSubjectsModel.js
+const mongoose = require('mongoose');
+
+const userSubjectsSchema = new mongoose.Schema({
+  userEmail: { type: String },
+  subjects: [{ name: String, credit: String, code: String }],
+});
+
+module.exports = mongoose.model('UserSubjects', userSubjectsSchema);
